34e40b17741beca6c0fe22c961ba140e3a2c985f
mohammed-hamdi-01 <mohammed.hamdi.01@outlook.fr>
2022-08-29 02:22:20 +0100

    third commit

---
+++ app/app/calc.py
@@ -0,0 +1,3 @@
+"""calculator func"""
+def add(x,y):
+    return x+y
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+"django command to wait for db to be available"
+from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+
+class Command(BaseCommand):
+    "django command to wait for db"
+    def handle(self, *args, **options):
+        self.stdout.write('waiting for database . . .')
+        db_up=False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error,OperationalError):
+                self.stdout.write('database unavailable, waiting 1 second')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('database is available'))        
